386fe176a6ffe205d2c9ffcf01893b97ebd89dea
tripathytelecom <help.santanu1@gmail.com>
2025-12-31 21:48:18 +0530

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -6,13 +6,17 @@ fetch("jobs.json?v=" + new Date().getTime())
   const results = document.getElementById("latest-results");
   const admit = document.getElementById("latest-admit");
 
+  jobs.innerHTML = "";
+  results.innerHTML = "";
+  admit.innerHTML = "";
+
   data.forEach((j, i) => {
     const li = document.createElement("li");
     li.innerHTML = `<a href="job-details.html?id=${i}">${j.job}</a>`;
 
     if (j.type === "job") jobs.appendChild(li);
-    if (j.type === "result") results.appendChild(li);
-    if (j.type === "admit") admit.appendChild(li);
+    else if (j.type === "result") results.appendChild(li);
+    else if (j.type === "admit") admit.appendChild(li);
   });
 
 });

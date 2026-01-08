a81f59ae48425a672f19e7b7398d1edc46b55ad4
tripathytelecom <help.santanu1@gmail.com>
2026-01-08 21:09:09 +0530

    Update tt-recharge.js

--- tt-recharge.js
+++ tt-recharge.js
@@ -1,35 +1,26 @@
 function sendRecharge(){
-
   let mobile = document.getElementById("mobile").value.trim();
   let amount = document.getElementById("amount").value.trim();
   let msg = document.getElementById("msg");
 
-  if(mobile.length !== 10){
-    msg.innerText = "❌ Invalid Mobile Number";
-    return;
-  }
-
-  if(amount <= 0){
-    msg.innerText = "❌ Invalid Amount";
-    return;
-  }
-
   msg.innerText = "⏳ Processing...";
 
   fetch("https://tripathytelecom.store/api/recharge/", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    mobile: mobile,
-    amount: amount,
-    operator: "AT"
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      mobile: mobile,
+      amount: amount,
+      operator: "AT"
+    })
   })
-})
   .then(res => res.text())
   .then(data => {
-    msg.innerText = "✅ Response: " + data;
+    msg.innerText = data;
   })
   .catch(err => {
-    msg.innerText = "❌ Server Error";
+    msg.innerText = "JS Error: " + err;
   });
 }
